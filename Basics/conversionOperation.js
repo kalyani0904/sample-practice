455d299d67fa997687c928c2933cfb24342a65c7
kalyani0904 <kalyanirathod09.09@gmail.com>
2025-04-25 19:02:45 +0000

    conversion Datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let value = Number(33);
+console.log(typeof value);
+
+// NaN not a number
+// "33" => 33
+// true => 1 false => 0
+// "33abc" => NaN
+ 
+let isAdult = 1 ;
+
+let booleanIsAdult = Boolean (isAdult)
+console.log(booleanIsAdult);
+
+// 1 => true  0 => false
+// "" => false
+// "hitesh" => true
+
+let str = 33
+let stringstr = String (str)
+console.log(typeof stringstr);
