8297aa17fbb62c547061755a5ea8ba96ac279f0a
kalyani0904 <kalyanirathod09.09@gmail.com>
2025-04-26 06:56:32 +0000

    basics operation

---
+++ Basics/operations.js
@@ -0,0 +1,27 @@
+let str1 ="hello"
+let str2 = " kalyani"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(true);
+// console.log(+"");
+
+let x = 3
+let y = x++
+console.log(x , y) 
+
+let a = 3;
+const b = ++a
+console.log(a ,b);
+
+
+
+
+
+
